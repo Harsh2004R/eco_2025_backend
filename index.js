340a5a7354f5027e53915dcca27a7360d33726f3
Harsh2004R <115461808+Harsh2004R@users.noreply.github.com>
2025-08-01 17:27:38 +0530

    Cors Added

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import connection from "./db.js";
 import dotenv from "dotenv";
+import cors from "cors";
 import userRouter from "./Routes/user.routes.js"
 dotenv.config();
 
 const app = express();
+app.use(cors());
 
+app.use(express.json());
 app.use("/api/user", userRouter)
 app.get("/", (req, res) => {
     res.send("Home...")
